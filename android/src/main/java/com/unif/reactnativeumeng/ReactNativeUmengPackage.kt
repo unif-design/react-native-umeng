0df82fa2a7d8f2c788f9fc45a329870ae2447f45
dream-god1 <382724935@qq.com>
2026-05-26 23:40:55 +0800

    feat(android): ReactNativeUmengPackage registers three TurboModules

---
+++ android/src/main/java/com/unif/reactnativeumeng/ReactNativeUmengPackage.kt
@@ -0,0 +1,49 @@
+package com.unif.reactnativeumeng
+
+import com.facebook.react.BaseReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.module.model.ReactModuleInfo
+import com.facebook.react.module.model.ReactModuleInfoProvider
+
+class ReactNativeUmengPackage : BaseReactPackage() {
+  override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
+    return when (name) {
+      UmengCommonModule.NAME -> UmengCommonModule(reactContext)
+      UmengShareModule.NAME -> UmengShareModule(reactContext)
+      UmengAnalyticsModule.NAME -> UmengAnalyticsModule(reactContext)
+      else -> null
+    }
+  }
+
+  override fun getReactModuleInfoProvider(): ReactModuleInfoProvider {
+    return ReactModuleInfoProvider {
+      mapOf(
+        UmengCommonModule.NAME to ReactModuleInfo(
+          UmengCommonModule.NAME,
+          UmengCommonModule::class.java.name,
+          false, // canOverrideExistingModule
+          false, // needsEagerInit
+          false, // isCxxModule
+          true,  // isTurboModule
+        ),
+        UmengShareModule.NAME to ReactModuleInfo(
+          UmengShareModule.NAME,
+          UmengShareModule::class.java.name,
+          false,
+          false,
+          false,
+          true,
+        ),
+        UmengAnalyticsModule.NAME to ReactModuleInfo(
+          UmengAnalyticsModule.NAME,
+          UmengAnalyticsModule::class.java.name,
+          false,
+          false,
+          false,
+          true,
+        ),
+      )
+    }
+  }
+}
